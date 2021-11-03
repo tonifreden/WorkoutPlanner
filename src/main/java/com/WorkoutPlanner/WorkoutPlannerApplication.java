881519db812a9d7d9c4aa150398a94b555da9f39
tonifreden <toni.freden@gmail.com>
2021-11-03 10:52:13 +0200

    First commit

---
+++ src/main/java/com/WorkoutPlanner/WorkoutPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.WorkoutPlanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/WorkoutPlanner/WorkoutPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.WorkoutPlanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
